bbc5cd301e6d68db4d11a8b1827729e60f43a1e3
josh mitchell <jshmtchll@gmail.com>
2021-03-26 18:17:10 -0500

    added user input capture/ small css changes

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 let userInputEl = document.getElementById("search");
 let formInputEl = document.getElementById("search-form");
+let searchBtn = document.getElementById("search-btn");
 
 
 
@@ -14,16 +15,14 @@ let formSubmit = function(event) {
     if (city) {
         userInputEl.value = "";
         console.log(city);
-        
-        
     }
     else{
         alert("Please enter a correct city");
     }
-    
 }
 
 
 
 formInputEl.addEventListener("submit", formSubmit);
+searchBtn.addEventListener("click", formSubmit);
 
